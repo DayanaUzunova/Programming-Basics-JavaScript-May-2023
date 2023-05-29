216c16bff6ab2dcac7533ae16bedb0f21a0fa1e3
Dayana Uzunova <daqnauzunova@abv.bg>
2023-05-29 14:52:54 +0300

    Add more exercises.
    Some more exercises with conditional statements.

---
+++ Conditional Statements - More Exercises/bonusScore_2.js
@@ -0,0 +1,22 @@
+function solve(input){
+    let score = Number(input[0]);
+    let bonus = 0.0;
+
+    if (score <= 100){
+        bonus += 5;
+    } 
+    else if (score > 1000){
+        bonus += (score * 10/100);
+    }
+     else if (score > 100){
+        bonus += (score * 20/100);
+    }
+    if (score % 2 == 0){
+        bonus += 1;
+    } 
+    else if (score % 10 == 5){
+        bonus += 2;
+    }
+    console.log(bonus);
+    console.log(bonus + score);
+}
---
+++ Conditional Statements - More Exercises/sumSeconds_1.js
@@ -0,0 +1,29 @@
+function solve(input){
+let firstTime = Number(input[0]);
+let secondTime = Number(input[1]);
+let thirdTime = Number(input[2]);
+
+let totalTime = firstTime + secondTime + thirdTime;
+
+let minutes = 0;
+let seconds = 0;
+
+if (totalTime >= 120){
+    minutes = 2;
+    seconds = totalTime - 120;
+}
+else if (totalTime >= 60){
+    minutes = 1;
+    seconds = totalTime - 60;
+}
+else {
+    seconds = totalTime;
+}
+if (seconds < 10){
+    console.log(`${minutes}:0${seconds}`);
+}
+else {
+    console.log(`${minutes}:${seconds}`);
+}
+}
+solve("")
